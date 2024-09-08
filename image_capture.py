c3f900197490c737019e0995d7e7d45a29110986
zackjonez123 <42856443+zackjonez123@users.noreply.github.com>
2024-09-08 17:34:07 -0600

    Add files via upload

---
+++ image_capture.py
@@ -0,0 +1,78 @@
+import cv2
+import os
+import imutils
+import time
+
+def main():
+    #state of doorway_lights on/off (L1 = on, L0 = off)
+    #picloop('empty_L1')
+    #picloop('empty_L0')
+    #picloop('closed_L1')
+    #picloop('closed_L0')
+    picloop('Zack_L1')
+    #picloop('Zack_L0')
+
+def grayscale(name):
+    # Load the input image
+    image = cv2.imread('/home/pi/code/captured_images/'+name+'.jpg')
+    # Use the cvtColor() function to grayscale the image
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gray_save = cv2.imwrite('/home/pi/code/captured_images/gray_'+name+'.jpg', gray_image)
+    # Use the cvtColor() function to threshold grayscale the image
+    thresh = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    thresh_save = cv2.imwrite('/home/pi/code/captured_images/thresh_'+name+'.jpg', thresh)
+
+def picloop(img_type):
+    #Take a certain number of pictures at a time (defined by the variable num)
+    #changes the name of the image file to avoid overwritting
+    name_count = 0
+    name = ''
+    #while loop to take 10 pictures at a time (b/c num is assigned to 10)
+    count = 0
+    num = 10
+    while count < num:
+        name_count += 1
+        name = img_type + '_doorway' + str(name_count)
+        pic(name)
+        count += 1
+        print(name)
+        grayscale(name)
+        print(count)
+    return None
+
+def pic(name):
+    #save_frame_camera_key(0, 'data/temp', 'camera_capture')
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    (grabbed, frame) = cap.read()
+    showimg = frame
+    cv2.waitKey(1)
+    time.sleep(0.3) # Wait 300 miliseconds
+    image = '/home/pi/code/captured_images/'+name+'.jpg'
+    cv2.imwrite(image, frame)
+    cap.release()
+    return None
+
+def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, window_name='frame'):
+    cap = cv2.VideoCapture(device_num)
+
+    if not cap.isOpened():
+        return
+
+    base_path = os.path.join(dir_path, basename)
+
+    n = 0
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow(window_name, frame)
+        key = cv2.waitKey(delay) & 0xFF
+        if key == ord('c'):
+            cv2.imwrite('{}_{}.{}'.format(base_path, n, ext), frame)
+            n += 1
+        elif key == ord('q'):
+            break
+
+    cv2.destroyWindow(window_name)
+
+if __name__ == '__main__':
+    main()

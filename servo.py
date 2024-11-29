4f351ffee8f7697f4f9ee1f73881fca88f647424
zackjonez123 <42856443+zackjonez123@users.noreply.github.com>
2024-11-29 15:29:45 -0700

    Add files via upload

---
+++ servo.py
@@ -0,0 +1,24 @@
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+pwm=GPIO.PWM(3, 50)
+pwm.start(0)
+
+def SetAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(3, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(3, False)
+	pwm.ChangeDutyCycle(0)
+
+def servo1():
+    SetAngle(90)
+    SetAngle(180)
+    SetAngle(90)
+    pwm.stop()
+    GPIO.cleanup()

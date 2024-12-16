f62acd83ee86eb8a38fa1c9d905bf75da72f8da3
zackjonez123 <42856443+zackjonez123@users.noreply.github.com>
2024-12-16 10:19:55 -0700

    Add files via upload

--- crop.py
+++ crop.py
@@ -1,7 +1,14 @@
 import cv2
 
 def crop(read_path):
-    
+    """ Crops an image into a 240x240 square for the Convolutional Neural Network (CNN)
+
+    Args:
+        read_path (string): path to the image
+
+    Returns:
+        cropped_img (image): cropped image (240x240)
+    """
     # Read input image
     input_img = cv2.imread(read_path)
     #print(input_img.shape)

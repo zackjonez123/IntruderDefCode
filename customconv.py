764976e245e766448030b051f50ed3f83929e615
zackjonez123 <42856443+zackjonez123@users.noreply.github.com>
2024-12-16 10:14:55 -0700

    Add files via upload
    Added docstrings and comments

--- customconv.py
+++ customconv.py
@@ -1,14 +1,22 @@
 """
-    ****** This is the manual convolutional pass for the IDS *********
-
-    Returns:
-        _type_: _description_
+    ****** This is the manual convolutional pass for the IDS ******
 """
 import numpy as n
 import cv2
 import os
 
 def conv(in_image, kernel):
+    """ Performs convolution between the input image and filter (kernel) image
+        as the kernel shifts across the input image. 
+        These values are stored in convolved_arr
+
+    Args:
+        in_image (image): captured image from the doorway
+        kernel (image): cropped image of the friendly occupant's eyes and nose
+
+    Returns:
+        convolved_arr (array): convolution results
+    """
     # Array of convolutions
     convolved_arr = []
     # Size of filter image
@@ -24,73 +32,71 @@ def conv(in_image, kernel):
 
     for i in range(height):
         for j in range(width):
-            splice = in_image[i : i + fil_height, j : j + fil_width] # splicing the input image
-            convolved = n.sum(n.multiply(splice, kernel))
+            splice = in_image[i : i + fil_height, j : j + fil_width] # splicing the input image by the kernel's dimensions
+            convolved = n.sum(n.multiply(splice, kernel)) # multiplying each pixel in the spliced input image with the pixels of the kernel image, then summing the products
             norm_convolved = convolved / 1000 # Normalize results 
-            convolved_arr.append(norm_convolved)
+            convolved_arr.append(norm_convolved) # store the convolution result into convolved_arr
 
     return convolved_arr
     
 def stats(in_image, kernel):
-    conv1 = conv(in_image, kernel)
-    #print(conv1)
-    max1 = n.max(conv1)
-    #min1 = n.min(conv1)
-    #print("Max value of"+descript+"is: ", max1)
-    #print("Min value of"+descript+"is: ", min1)
-    #average = n.sum(conv1) / len(conv1)
-    #print("Average value of"+descript+"is: ", average)
+    """ Runs conv() and calculates the maximum convolution result
+
+    Args:
+        in_image (image): captured image from the doorway
+        kernel (image): cropped image of the friendly occupant's eyes and nose
+
+    Returns:
+        max1 (float): maximum convolution value 
+    """
+    conv1 = conv(in_image, kernel) # run conv()
+    max1 = n.max(conv1) # calculate the max of convolved_arr
     
-    #stats = [max1, min1, average]
     return max1
 
 def evaluation(in_path, test_path, kernel):
     dir_list1 = os.listdir(in_path)
-    in_avmax = []
-    in_avmin = [] 
-    in_avavg = [] 
     inp = []
     dir_list2 = os.listdir(test_path)
-    test_avmax = [] 
-    test_avmin = [] 
-    test_avavg = []
     test = [] 
     # Run stats on all images
     for i in range(len(dir_list2)):
         test_img = cv2.imread(test_path+"\\"+dir_list2[i])
         t = stats(test_img, kernel)
-        # test_avmax.append(t[0])
-        # test_avmin.append(t[1])
-        # test_avavg.append(t[2])
-        #test.append(t[3])
         test.append(t)
     for j in range(len(dir_list1)):
         in_img = cv2.imread(in_path+"\\"+dir_list1[j])
         x = stats(in_img, kernel)
-        # in_avmax.append(x[0])
-        # in_avmin.append(x[1])
-        # in_avavg.append(x[2])
         inp.append(x)
-        #inp.append(x[4])
-
-    # tavg = n.sum(test_avavg) / len(test_avavg)
-    # tavgmax = n.sum(test_avmax) / len(test_avmax)
-    # tavgmin = n.sum(test_avmin) / len(test_avmin)
     
-    test_stats = n.min(test)
+    test_min = n.min(test)
+    test_max = n.max(test)
 
-    # inavg = n.sum(in_avavg) / len(in_avavg)
-    # inavgmax = n.sum(in_avmax) / len(in_avmax)
-    # inavgmin = n.sum(in_avmin) / len(in_avmin)
-    in_stats = n.max(inp)
+    in_max = n.max(inp)
+    in_min = n.min(inp)
 
-    print("Test Image stats are: ", test_stats)
-    print("Zack Image stats are: ", in_stats)
+    print("Test Image stats are: ", test_min, test_max)
+    print("Zack Image stats are: ", in_min, in_max)
 
 def decision(in_path, kernel, upper_thresh, lower_thresh):
-    img_read = cv2.imread(in_path)
-    y = stats(img_read, kernel)
-    #print(y)
+    """ Determines whether the maximum convolution value of the input image is in the accepable threshold.
+        The acceptable threshold was determined by taking the maximum and minimum of the max convolution values
+        for 100 "Friendly" images (max ==> upper_thresh, min ==> lower_thresh).
+        Any max convolution value outside this range is "Hostile"
+
+    Args:
+        in_path (string): path to the input image
+        kernel (image): cropped image of the friendly occupant's eyes and nose
+        upper_thresh (int): highest acceptable max convolution value
+        lower_thresh (int): lowest acceptable max convolution value
+
+    Returns:
+        boolean: False for "Friendly", True for "Hostile
+    """
+    img_read = cv2.imread(in_path) # Read the input image
+    y = stats(img_read, kernel) # Get the maximum of the convolution values
+
+    # check if within acceptable threshold
     if lower_thresh < y and y < upper_thresh:
         return False # Friendly
     else:
@@ -99,8 +105,8 @@ def decision(in_path, kernel, upper_thresh, lower_thresh):
 
 def main():
     in_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\classes\\croppedOccupied')
-    data_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\filters\\filter1.jpg')
-    test_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\testcrop')
+    data_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\filters\\filter2.jpg')
+    test_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\testcrop\\cole')
 
     # timg = cv2.imread(test_path)
     # print(n.shape(timg))
@@ -108,8 +114,8 @@ def main():
     #******** Determining threshold **********
     #zack_image = cv2.imread(in_path)
     kernel = cv2.imread(data_path)
-    upper_thresh = 710
-    lower_thresh = 620
+    upper_thresh = 715
+    lower_thresh = 625
     # dir_list1 = os.listdir(in_path)
     # dir_list2 = os.listdir(test_path)
     # for i in range(len(dir_list2)):

843744ba8fa31582ec8e107504e3f5645e73b5f9
zackjonez123 <42856443+zackjonez123@users.noreply.github.com>
2024-12-16 10:15:51 -0700

    Update customconv.py

--- customconv.py
+++ customconv.py
@@ -54,30 +54,6 @@ def stats(in_image, kernel):
     
     return max1
 
-def evaluation(in_path, test_path, kernel):
-    dir_list1 = os.listdir(in_path)
-    inp = []
-    dir_list2 = os.listdir(test_path)
-    test = [] 
-    # Run stats on all images
-    for i in range(len(dir_list2)):
-        test_img = cv2.imread(test_path+"\\"+dir_list2[i])
-        t = stats(test_img, kernel)
-        test.append(t)
-    for j in range(len(dir_list1)):
-        in_img = cv2.imread(in_path+"\\"+dir_list1[j])
-        x = stats(in_img, kernel)
-        inp.append(x)
-    
-    test_min = n.min(test)
-    test_max = n.max(test)
-
-    in_max = n.max(inp)
-    in_min = n.min(inp)
-
-    print("Test Image stats are: ", test_min, test_max)
-    print("Zack Image stats are: ", in_min, in_max)
-
 def decision(in_path, kernel, upper_thresh, lower_thresh):
     """ Determines whether the maximum convolution value of the input image is in the accepable threshold.
         The acceptable threshold was determined by taking the maximum and minimum of the max convolution values
@@ -101,50 +77,3 @@ def decision(in_path, kernel, upper_thresh, lower_thresh):
         return False # Friendly
     else:
         return True # Hostile
-
-
-def main():
-    in_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\classes\\croppedOccupied')
-    data_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\filters\\filter2.jpg')
-    test_path = ('C:\\Users\\kelly\\Desktop\\IDEs and Sims\\IntruderDef\\pics\\testcrop\\cole')
-
-    # timg = cv2.imread(test_path)
-    # print(n.shape(timg))
-
-    #******** Determining threshold **********
-    #zack_image = cv2.imread(in_path)
-    kernel = cv2.imread(data_path)
-    upper_thresh = 715
-    lower_thresh = 625
-    # dir_list1 = os.listdir(in_path)
-    # dir_list2 = os.listdir(test_path)
-    # for i in range(len(dir_list2)):
-    #     path2 = test_path+"\\"+dir_list2[i]
-    #     print(decision(path2, kernel, threshold))
-
-    # for j in range(len(dir_list2)):
-    #     path1 = in_path+"\\"+dir_list1[j]
-    #     print(decision(path1, kernel, threshold))
-
-    evaluation(in_path, test_path, kernel)
-    # print(stats(zack_image, kernel, "Zack"))
-
-    # test_image = cv2.imread(test_path)
-    # print(stats(zack_image, kernel, "Zack Input"))
-    # print(stats(test_image, kernel, "Test Input"))
-
-
-
-# *********** Notes *************
-# (w/ Filter cropped10.jpg)
-# - between Zack and Test images, the averages and mins are similar, but the maxs are:
-# Test avg max =717, Zack avg max = 775
-# The max of maximums for test are 746, the minimum of maximums for Zack are 758
-
-#11/16/24
-# Test Image stats are:  846.936
-# Zack Image stats are:  804.111
-# w/ cropv22.jpg
-
-if __name__ == '__main__':
-    main()

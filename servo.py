9261b595a18c55181b9dbc3a1e6395dd206ce4b3
zackjonez123 <42856443+zackjonez123@users.noreply.github.com>
2024-12-28 15:06:20 -0700

    Update servo.py

--- servo.py
+++ servo.py
@@ -9,6 +9,11 @@ pwm=GPIO.PWM(3, 50)
 pwm.start(0)
 
 def SetAngle(angle):
+	""" Turns the servo by a certain angle
+
+	Args:
+		angle (int): desired angle for the servo to turn
+	"""
 	duty = angle / 18 + 2
 	GPIO.output(3, True)
 	pwm.ChangeDutyCycle(duty)
@@ -17,6 +22,8 @@ def SetAngle(angle):
 	pwm.ChangeDutyCycle(0)
 
 def servo1():
+	""" Calls SetAngle, moving the servo motor by 90 degrees, then back 90 degrees
+	"""
     SetAngle(90)
     SetAngle(180)
     SetAngle(90)
